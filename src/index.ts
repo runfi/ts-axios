89542689d3e74fe915bb895fecd6cc241d62cc2c
runfi <lancelotdangao@163.com>
2020-02-28 21:55:25 +0800

    .

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { AxiosRequestConfig } from './types'
+import xhr from './xhr'
+
+function axios(config: AxiosRequestConfig): void {
+  // TODO
+  xhr(config)
+  xhr(config)
+}
+
+export default axios
