6f9448c0031388ac57bd9e2ee48e30ef81475ecd
runfi <lancelotdangao@163.com>
2020-03-01 23:08:07 +0800

    初步编写example

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'post'
+  | 'POST'
+  | 'options'
+  | 'OPTIONS'
+  | 'head'
+  | 'HEAD'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
